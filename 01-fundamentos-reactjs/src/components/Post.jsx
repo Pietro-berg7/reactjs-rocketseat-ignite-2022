384ad0fc5dd83155eae391c955f93aaded175c3f
Pietro <pietrovb25@gmail.com>
2024-02-20 18:17:36 -0300

    feat: add a component for avatar images

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,4 +1,5 @@
-import Comment from "./Comment";
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/Pietro-berg7.png"
-          />
+          <Avatar src="https://github.com/Pietro-berg7.png" />
           <div className={styles.authorInfo}>
             <strong>Pietro Berganaschi</strong>
             <span>Web Developer</span>
